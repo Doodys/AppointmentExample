ea8addfc2ad0abda82192a641ed8ad3a67c855f9
Mike <dudys.michal@gmail.com>
2022-12-13 21:23:21 +0100

    chore: Prepare ground for booking logic

--- DoctorAppointment.Client/Models/Appointment.cs
+++ DoctorAppointment.Client/Models/Appointment.cs
@@ -2,7 +2,6 @@
 
 public class Appointment
 {
-    public int Id { get; set; } = 0;
     public DateTime Date { get; set; }
     public string Hour { get; set; }
     public int UserId { get; set; }
